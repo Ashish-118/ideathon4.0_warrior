b2f62ba56708e9676a3b352f0248eff4bbc73324
Ashish <ashish@192.168.1.6>
2024-12-14 18:22:57 +0530

    created model for pyq

---
+++ back_end/src/models/PYQ.model.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose"
+
+
+
+const pyqSchema = new Schema(
+    {
+        forYear: {
+            type: String,
+            required: true,
+        },
+        courseCode: {
+            type: String,
+            required: true,
+        },
+        paperPdf: {
+            type: String,
+            required: true,
+        },
+        solutionPdf: {
+            type: String,
+            required: true,
+        },
+        solutionVideo: {
+            type: String,
+            required: true,
+        },
+        paperYear: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        sentByAdmin: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    }, { timestamps: true }
+
+)
+
+
+
+export const PYQ = mongoose.model("PYQ", pyqSchema)
